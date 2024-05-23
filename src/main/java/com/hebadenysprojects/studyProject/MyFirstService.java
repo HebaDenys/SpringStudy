659011336bbd83c28e1a7f32409ce2dd6b918d53
Denys Heba <hdenys.work@gmail.com>
2024-05-23 17:45:22 +0100

    added some code to try some ways to inject the beans and see how it works the enviroment Object of Spring

--- src/main/java/com/hebadenysprojects/studyProject/MyFirstService.java
+++ src/main/java/com/hebadenysprojects/studyProject/MyFirstService.java
@@ -2,53 +2,48 @@ package com.hebadenysprojects.studyProject;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.stereotype.Service;
 
+// Questa classe rappresenta il servizio principale dell'applicazione, MyFirstService.
+// È annotato con @Service per indicare a Spring di gestirlo come un componente all'interno del contesto dell'applicazione.
+// Questo significa che Spring sarà responsabile della creazione e della gestione delle istanze di questa classe.
 @Service
+// @PropertySources consente di specificare più file di proprietà da cui caricare le proprietà.
+@PropertySources({@PropertySource("classpath:custom.properties"), @PropertySource("classpath:custom2.properties")})
 public class MyFirstService {
 
+    // Questo campo rappresenta una dipendenza di tipo FirstClass.
+    private final FirstClass firstClass;
 
+    // Le seguenti annotazioni @Value vengono utilizzate per iniettare valori di proprietà da file di configurazione esterni.
+    // Questi valori sono configurati nei file custom.properties e custom2.properties.
+    @Value("${my.prop}")
+    private String myProp;
+    @Value("${my.custom.property}")
+    private int myProp2;
 
-//    @Autowired
-//    @Qualifier("bean1")
-//    private FirstClass firstClass;
-
-    private FirstClass firstClass;
-    private Environment environment;
-
+    // L'annotazione @Autowired indica a Spring di iniettare automaticamente un'istanza di FirstClass quando viene creato un'istanza di MyFirstService.
+    // L'annotazione @Qualifier viene utilizzata per specificare quale bean di FirstClass deve essere iniettato, nel caso ci siano più implementazioni di FirstClass disponibili.
     @Autowired
-    public void injectDependencies(
-           @Qualifier("bean2") FirstClass firstClass
-    ){
-      this.firstClass = firstClass;
+    public MyFirstService(@Qualifier("bean2") FirstClass firstClass) {
+        this.firstClass = firstClass;
     }
 
-
-//    private final FirstClass firstClass;
-
-//    @Autowired
-//    public MyFirstService(
-////            @Qualifier("bean2")
-//            FirstClass firstClass) {
-//        this.firstClass = firstClass;
-//    }
-
-    public String tellAStory(){
-            return STR."the dependency is saying \{firstClass.sayHello()}";
-    }
-    public String getJavaVersion(){
-            return STR."The java Version we are using is --> \{environment.getProperty("java.version")}";
-    }
-    public String getOsVersion(){
-            return STR."The os name we are using is --> \{environment.getProperty("os.name")}";
-    }
-    public String getCustomProperty(){
-            return STR."The os name we are using is --> \{environment.getProperty("my.custom.property")}";
+    // Questo metodo restituisce una stringa che racconta una storia utilizzando il metodo sayHello() dell'istanza di FirstClass.
+    public String tellAStory() {
+        return "The dependency is saying: " + firstClass.sayHello();
     }
 
-@Autowired
-    public void setEnvironment(Environment environment) {
-        this.environment = environment;
+    // Questi metodi forniscono accesso alle proprietà configurate tramite le annotazioni @Value.
+    // Restituiscono i valori delle proprietà recuperate dai file di configurazione.
+    public String getMyProp() {
+        return myProp;
+    }
+
+    public int getMyProp2() {
+        return myProp2;
     }
 }
--- src/main/java/com/hebadenysprojects/studyProject/StudyProjectApplication.java
+++ src/main/java/com/hebadenysprojects/studyProject/StudyProjectApplication.java
@@ -2,27 +2,29 @@ package com.hebadenysprojects.studyProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class StudyProjectApplication {
 
 	public static void main(String[] args) {
-		var context = SpringApplication.run(StudyProjectApplication.class, args);
+		// Avvia l'applicazione Spring e ottiene il contesto dell'applicazione
+		ApplicationContext context = SpringApplication.run(StudyProjectApplication.class, args);
 
+		// Ottiene e utilizza il bean FirstClass
+		FirstClass firstClass = context.getBean("firstClass", FirstClass.class);
+		System.out.println("Message from FirstClass: " + firstClass.sayHello());
 
-		FirstClass firstClass = context.getBean("firstClass",FirstClass.class);
-		System.out.println(firstClass.sayHello());
+		// Ottiene e utilizza il bean SecondClass rinominato
+		SecondClass secondClass = context.getBean("renamedBean", SecondClass.class);
+		System.out.println("Message from SecondClass: " + secondClass.sayByeBye());
 
-		SecondClass secondClass = context.getBean("renamedBean",SecondClass.class);
-		System.out.println(secondClass.sayByeBye());
-
+		// Ottiene e utilizza il servizio MyFirstService
 		MyFirstService myFirstService = context.getBean(MyFirstService.class);
+		System.out.println("MyFirstService Story: " + myFirstService.tellAStory());
+		System.out.println("MyFirstService Property myProp: " + myFirstService.getMyProp());
+		System.out.println("MyFirstService Property myProp2: " + myFirstService.getMyProp2());
 
-		System.out.println(myFirstService.tellAStory());
-		System.out.println(myFirstService.getJavaVersion());
-		System.out.println(myFirstService.getOsVersion());
-		System.out.println(myFirstService.getCustomProperty());
-
+		// L'applicazione è stata avviata con successo e tutte le componenti Spring sono state testate
 	}
-
 }
